3a9b4e99f21c22e44146177cd3ac206583b9ba24
Pawel <pspychalski@gmail.com>
2016-03-27 20:20:00 +0200

    two button mode

--- servo_tester.ino
+++ servo_tester.ino
@@ -3,34 +3,42 @@
 Servo servo;
 
 const int PIN_BUTTON = 6;
+const int PIN_BUTTON_2 = 7;
 const int PIN_SERVO = 5;
 
-int pos = 1500;
+#define MIN_WIDTH 1000
+#define MAX_WIDTH 2000
 
-int prevButtonState = HIGH;
-int currentButtonState = HIGH;
+int pos = 1500;
 
 void setup() {
     servo.attach(PIN_SERVO);
     servo.writeMicroseconds(pos);
     pinMode(PIN_BUTTON, INPUT_PULLUP);
+    pinMode(PIN_BUTTON_2, INPUT_PULLUP);
 }
 
 void loop() {
-  
-  currentButtonState = digitalRead(PIN_BUTTON);
-  
-  if (currentButtonState == LOW && prevButtonState == HIGH) {
-    pos = pos + 500;
-    
-    if (pos > 2000) {
-       pos = 1000; 
+
+  if (digitalRead(PIN_BUTTON) == LOW) {
+    pos = pos + 10;
+
+    if (pos > MAX_WIDTH) {
+       pos = MAX_WIDTH; 
     }
-    
+
+    servo.writeMicroseconds(pos);
+  }
+
+  if (digitalRead(PIN_BUTTON_2) == LOW) {
+    pos = pos - 10;
+
+    if (pos > MIN_WIDTH) {
+       pos = MIN_WIDTH; 
+    }
+
     servo.writeMicroseconds(pos);
   }
   
-  prevButtonState = currentButtonState;
-  
-  delay(100);
+  delay(20);
 }
